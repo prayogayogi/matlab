4e6b5ce09b5fbca8a26207591c1497ed5ed9cbc2
Yogi Prayoga <75689285+prayogayogi@users.noreply.github.com>
2021-01-17 23:41:55 +0700

    error

--- detekFoto.m
+++ detekFoto.m
@@ -1,16 +1,1 @@
-[filename,pathname]=uigetfile('*.*','Select the Input Grayscal Image');
-filewithpath=strcat(pathname,filename);
-Img=imread(filewithpath);
-faceDetector=vision.CascadeObjectDetector;
-faceDetector.MergeThreshold=10;
-bboxes = faceDetector(Img);
-if ~isempty (bboxes)
-Imgf = insertObjectAnnotation(Img,'rectangle',bboxes,'Face','LineWidth',3);
-imshow(Imgf)
-title('Detected faces');
-else
-position=[0 0]; %[x y]
-label='No face Detected';
-Imgn = insertText(Img,position,label,'FontSize',25,'BoxOpacity',1);
-imshow(Imgn)
-end
+Erorr
